f2769be7efeb84844bacda3af31fb9ba21ea3e75
AhmedSherif20 <aymanmard12@gmail.com>
2021-08-26 22:17:29 +0200

    project done

--- js/script.js
+++ js/script.js
@@ -28,3 +28,39 @@ $(document).ready(function () {
         }
     })
 });
+
+let navBar = document.querySelector("nav")
+let navBarLinks = document.querySelectorAll(".nav-link")
+
+let videoShow = document.querySelector(".video-show")
+let video = document.querySelector("#video")
+let videoPlay = document.querySelector("#videoPlay")
+let videoClose = document.querySelector("#videoClose")
+
+window.onscroll = () => {
+    let windowScroll = window.scrollY
+    if (windowScroll > 500) {
+        navBar.style.backgroundColor = "#130f40"
+    } else {
+        navBar.style.backgroundColor = "transparent"
+    }
+}
+
+navBar.addEventListener("click", (e) => {
+    for (let i = 0; i < navBarLinks.length; i++) {
+        navBarLinks[i].classList.remove("active")
+    }
+    e.target.classList.add("active")
+})
+
+videoPlay.addEventListener("click", () => {
+    videoShow.style.display = "flex"
+    video.setAttribute("src", "https://www.youtube.com/embed/TCMnrssX1NE")
+    document.body.style.overflow = "hidden"
+
+})
+videoClose.addEventListener("click", () => {
+    videoShow.style.display = "none"
+    video.setAttribute("src", "")
+    document.body.style.overflow = "auto"
+})
